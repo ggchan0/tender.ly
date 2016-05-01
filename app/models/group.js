6da8f36576bd9d942808dba6f21ca94074b1e052
Mitchell <mover333@gmail.com>
2016-04-30 18:32:25 -0700

    fixed group and emails

--- app/models/group.js
+++ app/models/group.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var group = mongoose.Schema({
    owner : String,
    name : String,
-   emails : [String]
+   emails : {type : [String], unique: true}
 });
 
 var group = mongoose.model('Group', group);
